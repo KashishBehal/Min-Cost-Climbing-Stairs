1cc731f40e6b331eba4f9b5908ded2300ed066f2
Kashish Behal <113112253+KashishBehal@users.noreply.github.com>
2023-10-04 19:27:57 +0530

    Min Cost Climbing Stairs.cpp

---
+++ Min Cost Climbing Stairs.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+  int solve(vector<int>&cost, int N , vector<int>&dp){
+      if(N==0){
+          return cost[0];
+      }
+      if(N==1){
+          return cost[1];
+      }
+      if(dp[N]!=-1){
+          return dp[N];
+      }
+      dp[N]=cost[N]+min(solve(cost , N-1  , dp) , solve(cost , N-2 , dp));
+      return dp[N];
+  }
+    int minCostClimbingStairs(vector<int>&cost ,int N) {
+    vector<int>dp(N+1 , -1);
+    int ans=min(solve(cost  , N-1  , dp) , solve(cost , N-2 , dp));
+    return ans;
+    }
+};
